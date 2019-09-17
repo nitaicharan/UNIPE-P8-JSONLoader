04305d1069c539a8adc5e1b97f68d96ad7bd63f6
Nitai Charan <nitaicharan@gmail.com>
2019-09-17 20:58:26 -0300

    feature#168529033 - Realizar prototipo de envio para Elasticsearch
    Realizado prototipo de envio

--- src/index.js
+++ src/index.js
@@ -6,19 +6,33 @@
 
 const fs = require('fs');
 const reader = require('readjson');
+const { Client } = require('@elastic/elasticsearch');
+const client = new Client({ node: 'http://localhost:9200' });
 
-
-onInit();
-
-var files = fs.readdirSync('tmp/');
+const files = fs.readdirSync('tmp/');
 
 files.forEach(file => {
-    var objects = new Array();
-    file = 'tmp/'+file;
-    reader(file, (err, object)=>{
-        objects.push(object);
+    var path = 'tmp/'+ file;
+    reader(path, (err, object)=>{
+        run(object, file).catch(console.log)
     });
 });
 
-function onInit(){
+
+async function run(object, name){
+    name = name.split('.',1)
+    name = name[0].split('-',2)
+    name = name[1];
+    await client.bulk({
+        index: 'gastos',
+        type: '_doc',
+        id: name,
+        body: object
+    });
+
+    const { body } = await client.get({
+        index: 'gastos',
+        id: '1'
+    })
+    //console.log(body)
 }
